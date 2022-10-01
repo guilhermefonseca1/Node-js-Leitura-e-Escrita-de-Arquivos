72c5cb8f12a6c696a4172f843eadf307062b4636
Guilherme Soares <guilherme.limasoares@hotmail.com>
2022-10-01 18:29:08 -0300

    criando caminho(buffer) para acessar o arquivo .json, o qual contém os planetas

--- src/utils/fsUtils.js
+++ src/utils/fsUtils.js
@@ -1,11 +1,12 @@
 const fs = require('fs').promises;
+const path = require('path')
 
 async function readMissionsData() {
     try {
-        const data = await fs.readFile('../../data/missions.json')
+        const data = await fs.readFile(path.resolve(__dirname, '../../data/missions.json'))
         console.log(data)
     } catch(error) {
-        console.error(`Erro na leitura do arquivo: ${error}`)
+        console.error(`Erro na leitura do arquivo: ${error}`)    
     }
 }
 

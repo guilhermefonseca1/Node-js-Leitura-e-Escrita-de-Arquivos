1d76bcfb86e84237e9119f79c88cbe0b1f244698
Guilherme Soares <guilherme.limasoares@hotmail.com>
2022-10-02 20:42:26 -0300

    consertando erros de lint

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const readline = require('readline-sync')
-const { readMissionsData, writeNewMissionDate } = require('./utils/fsUtils');
+const readline = require('readline-sync');
+const { writeNewMissionDate } = require('./utils/fsUtils');
 
 async function main() {
-    const name = readline.question('Qual é o nome da missão?')
-    const year = readline.questionInt('Qual é o ano da missão?')
-    const country = readline.question('Qual é o país da missão?')
-    const destination = readline.question('Qual é o destino da missão?')
+    const name = readline.question('Qual é o nome da missão?');
+    const year = readline.questionInt('Qual é o ano da missão?');
+    const country = readline.question('Qual é o país da missão?');
+    const destination = readline.question('Qual é o destino da missão?');
 
-    const newMission = { name, year, country, destination }
-    writeNewMissionDate(newMission)
-    console.log('Nova missão registrada com sucesso!')
+    const newMission = { name, year, country, destination };
+    writeNewMissionDate(newMission);
+    console.log('Nova missão registrada com sucesso!');
     // writeNewMissionDate({
     //         "name": "Tryber",
     //         "year": "2001",
@@ -18,5 +18,4 @@ async function main() {
     // })
 }
 
-
-main()
+main();
